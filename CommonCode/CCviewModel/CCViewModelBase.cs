c0a2289428fc11cb0e236e0fc191ba00f9aa3793
HapiFive <1143910315@qq.com>
2020-05-12 22:54:56 +0800

    ViewModelBase

---
+++ CommonCode/CCviewModel/CCViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CommonCode.CCviewModel {
+    public class CCViewModelBase : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool Set<T>(ref T field, T newValue = default, [CallerMemberName] string propertyName = null) {
+            if (EqualityComparer<T>.Default.Equals(field, newValue)) {
+                return false;
+            }
+            field = newValue;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        protected bool Update([CallerMemberName] string propertyName = null) {
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
