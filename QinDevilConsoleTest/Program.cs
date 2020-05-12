e02e3a3abc61b9998b61faac839062ef5dc52621
HapiFive <1143910315@qq.com>
2020-05-12 18:56:20 +0800

    一些测试类

--- QinDevilConsoleTest/Program.cs
+++ QinDevilConsoleTest/Program.cs
@@ -1,9 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace QinDevilConsoleTest {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Test1();
+        }
+        private static void Test1() {
+            List<byte> b = new List<byte>();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 100000; i++) {
+                b.Add((byte)i);
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds+"ms");
+            stopwatch.Restart();
+            for (int i = 0; i < 100000; i++) {
+                b.RemoveRange(0, 1);
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds + "ms");
         }
     }
 }
--- QinDevilTest/MainWindow.xaml.cs
+++ QinDevilTest/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,30 @@ namespace QinDevilTest {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("TextBox_GotFocus");
+        }
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("TextBox_LostFocus");
+        }
+        private void Window_GotFocus(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("Window_GotFocus");
+        }
+        private void Window_LostFocus(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("Window_LostFocus");
+        }
+        private void TextBox_GotFocus_1(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("TextBox_GotFocus_1");
+        }
+        private void TextBox_LostFocus_1(object sender, RoutedEventArgs e) {
+            Debug.WriteLine("TextBox_LostFocus_1");
+        }
+        private void Window_Activated(object sender, EventArgs e) {
+            Debug.WriteLine("Window_Activated");
+        }
+        private void Window_Deactivated(object sender, EventArgs e) {
+            Debug.WriteLine("Window_Deactivated");
         }
     }
 }
