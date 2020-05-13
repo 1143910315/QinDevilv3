376a976fc01ef2f82fdbc06545cfd528c9d5af4b
HapiFive <1143910315@qq.com>
2020-05-13 23:41:41 +0800

    日志管理

---
+++ CommonCode/CClog/LogFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CommonCode.CClog {
+    public class LogFile {
+        private readonly StreamWriter sw;
+        private readonly TextWriter tw;
+        public LogFile(string path) {
+            sw = File.AppendText(path);
+            tw = TextWriter.Synchronized(sw);
+        }
+        public void Generate(string log) {
+            tw.WriteLineAsync(log).Wait();
+        }
+        public void Flush() {
+            tw.Flush();
+        }
+        ~LogFile() {
+            tw.Flush();
+        }
+    }
+}
