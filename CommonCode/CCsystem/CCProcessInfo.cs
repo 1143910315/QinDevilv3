614b48c88d5b77b7ea060804927e546fa88fc3f8
HapiFive <1143910315@qq.com>
2020-05-13 23:46:12 +0800

    获取进程信息

---
+++ CommonCode/CCsystem/CCProcessInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CommonCode.CCsystem {
+    public class CCProcessInfo {
+        [DllImport("Kernel32.dll", EntryPoint = "QueryFullProcessImageNameA")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool QueryFullProcessImageNameA(IntPtr hProcess, int dwFlags, [Out] StringBuilder lpExeName, ref int lpdwSize);
+        public static string GetProcessPath(Process process) {
+            if (process != null) {
+                int i = 0;
+                int length;
+                StringBuilder stringBuilder;
+                do {
+                    i++;
+                    length = i * 260;
+                    stringBuilder = new StringBuilder(length);
+                    _ = QueryFullProcessImageNameA(process.Handle, 0, stringBuilder, ref length);
+                    if (length == 0) {
+                        return "";
+                    }
+                } while (i * 260 == length);
+                return stringBuilder.ToString();
+            } else {
+                return "";
+            }
+        }
+    }
+}
